6c7d54efa75e43721f52321a2506773ebf602f0a
shashank7373 <shashudex@gmail.com>
2025-08-12 12:08:41 +0530

    Initial commit: Add .confg and .themes

---
+++ .config/doom/custom.el
@@ -0,0 +1,14 @@
+(put 'customize-face 'disabled nil)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(hl-line ((t (:inherit highlight :background "#393836"))))
+ '(line-number-current-line ((t (:inherit line-number)))))
